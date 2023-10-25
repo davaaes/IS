7b9627631fea59aa4b4a2865e86513ac4158f8c8
juan garcia <j.gdans@udc.es>
2023-10-25 18:14:46 +0200

    prueba

---
+++ archivoscsv/lectorcsv.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+dfexcel = pd.read_excel("./practica1/archivoscsv/housing.xlsx")
+
+print(dfexcel)
+
+dfcsv = pd.read_csv("./practica1/archivoscsv/housingcsv.csv")
+
+print(dfcsv)
