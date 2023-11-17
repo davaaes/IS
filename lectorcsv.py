0d9a30b711281f63d317d040ec3ff512528175ef
davaes <david.vaamonde.estevez@udc.es>
2023-11-17 16:27:35 +0100

    Revert "Revert "Merge branch 'master' of https://github.com/davaaes/IS into INTERFAZ_GRAFICA""
    This reverts commit d43b206140d579769c114818de894e0216b3df24.

--- lectorcsv.py
+++ lectorcsv.py
@@ -1,7 +1,8 @@
 import pandas as pd
-def leer_excel():
-    dfexcel = pd.read_excel("./archivoscsv\housing.xlsx")
-    return dfexcel
-def leer_csv():
-    dfcsv = pd.read_csv("./archivoscsv\housingcsv.csv")
-    return dfcsv
+def leer_archivo(archivo):
+    if archivo.endswith('.csv'):
+        return pd.read_csv(archivo)
+    elif archivo.endswith('.xlsx') or archivo.endswith('.xls'):
+        return pd.read_excel(archivo)
+    else:
+        raise ValueError("Formato de archivo no compatible. Debe ser CSV o Excel.")
