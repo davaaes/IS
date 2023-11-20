5b4240c58098b70776cfa1fdd0b8c59fd6758fcb
davaes <david.vaamonde.estevez@udc.es>
2023-11-20 17:48:23 +0100

    añadir tabla y leer sqlite

--- MAIN.py
+++ MAIN.py
@@ -1,12 +1,11 @@
 import tkinter as tk
 from tkinter import filedialog
+from tkinter import ttk
 
-global archivo
+import lectorcsv as l
 
 def cargar_archivo():
-    
     global archivo
-
     archivo = filedialog.askopenfilename(title="Seleccionar archivo")
     if archivo:
         entrada_texto.config(state='normal')  # Habilitar el cuadro de texto
@@ -14,37 +13,48 @@ def cargar_archivo():
         entrada_texto.insert(0, archivo)  # Insertar la ruta del archivo seleccionado
         entrada_texto.config(state='disabled')  # Volver a deshabilitar el cuadro de texto
 
+        # Utilizar la ruta del archivo con la función leer_archivo
+        dataframe = l.leer_archivo(archivo)
 
+        if dataframe is not None:
+            print("Archivo leído exitosamente.")
+
+            # Crear un Treeview para mostrar la tabla
+            treeview = ttk.Treeview(ventana)
+            treeview["columns"] = tuple(dataframe.columns)
+            
+            # Configurar encabezados
+            for column in dataframe.columns:
+                treeview.heading(column, text=column)
+            
+            # Insertar datos
+            for i, row in dataframe.iterrows():
+                treeview.insert("", i, values=tuple(row))
+
+            # Agregar barras de desplazamiento vertical y horizontal directamente en el Treeview
+            yscroll = ttk.Scrollbar(ventana, orient="vertical", command=treeview.yview)
+            yscroll.pack(side="right", fill="y")
+            treeview.configure(yscrollcommand=yscroll.set)
+
+            xscroll = ttk.Scrollbar(ventana, orient="horizontal", command=treeview.xview)
+            xscroll.pack(side="bottom", fill="x")
+            treeview.configure(xscrollcommand=xscroll.set)
+            
+            # Centrar la tabla en la ventana
+            treeview.pack(pady=10, padx=10)
+            
+# Crear ventana y otros elementos
 ventana = tk.Tk()
 
 ancho_pantalla = ventana.winfo_screenwidth()
 alto_pantalla = ventana.winfo_screenheight()
-ventana.geometry(f"{ancho_pantalla}x{alto_pantalla}") 
+ventana.geometry(f"{ancho_pantalla}x{alto_pantalla}")
 
-entrada_texto = tk.Entry(ventana,state='disabled', width=40)
+entrada_texto = tk.Entry(ventana, state='disabled', width=40)
 entrada_texto.pack(pady=10)
 
 # Botón para cargar un archivo
 boton_cargar = tk.Button(ventana, text="Cargar Archivo", command=cargar_archivo)
 boton_cargar.pack(pady=10)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 ventana.mainloop()
