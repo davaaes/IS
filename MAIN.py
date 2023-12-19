bdc0cc2a187ccf638415f63f7bf8e8316fcafbd4
davaes <david.vaamonde.estevez@udc.es>
2023-12-19 17:19:39 +0100

    merge de grafica con master

--- MAIN.py
+++ MAIN.py
@@ -2,14 +2,20 @@ import tkinter as tk
 from tkinter import filedialog
 from tkinter import ttk
 import sys
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import lector as l
 from modelos import *
+import joblib
+from decimal import Decimal, getcontext
 
-global estados_checkbuttons, opcion_seleccionada 
+
+global estados_checkbuttons, opcion_seleccionada, list_vi
 
 
 def mostrar_modelo():
-    global estados_checkbuttons, opcion_seleccionada,columnas,dataframe
+    global estados_checkbuttons, opcion_seleccionada,columnas,dataframe,interc,coef,list_vi
+    n=1
+    limpiar_interfaz(2)
     lista_vo=[opcion_seleccionada.get()]
     list_vi=[]
     for i in range(len(estados_checkbuttons)):
@@ -26,25 +32,120 @@ def mostrar_modelo():
     if "ocean_proximity" in list_vi or "ocean_proximity" in lista_vo:
         tk.messagebox.showerror("Error", "La variable ocean_proximity no puede usarse como variable ya que es una cadena de texto.")
         return
-    regresion(lista_vo,list_vi,dataframe)
+    fig,error,formula,interc,coef= regresion(lista_vo, list_vi, dataframe)
+    predicciones(list_vi)
+    mostrar_formula(error,formula,n)
+    plot_grafico(fig)
 
+def mostrar_formula(error,formula,n):
+    contenido = f"Formula={formula}\nError={error}"
+
+    if n==1:
+        formula_error = tk.Label(frame_grafica, text=f"Formula={formula}\nError={error}")
+    else:
+        formula_error = tk.Label(text=f"Formula={formula}\nError={error}")
+
+    formula_error.pack()
+
+def plot_grafico(fig):
+    fig.set_size_inches(3, 2)
+    # Crea el lienzo de Tkinter para la figura de Matplotlib
+    canvas = FigureCanvasTkAgg(fig, master=frame_grafica)
+    canvas.draw()
+    canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=1)
 
 def guardar_modelo():
-    global dataframe, opcion_seleccionada,estados_checkbuttons
-    lista_vo=[opcion_seleccionada.get()]
-    list_vi=[]
+    global dataframe, opcion_seleccionada, estados_checkbuttons
+
+    lista_vo = [opcion_seleccionada.get()]
+    list_vi = []
     for i in range(len(estados_checkbuttons)):
-        if estados_checkbuttons[i].get()==True:
+        if estados_checkbuttons[i].get() == True:
             list_vi.append(columnas[i])
-    print(texto)
-    regresion(lista_vo,list_vi,dataframe,str(texto.get()) +".png")
+
+    if len(list_vi) == 0:
+        tk.messagebox.showerror("Error", "Seleccione al menos una variable independiente.")
+        return
+
+    if not opcion_seleccionada.get():
+        tk.messagebox.showerror("Error", "Seleccione una variable objetivo.")
+        return
+
+    if "ocean_proximity" in list_vi or "ocean_proximity" in lista_vo:
+        tk.messagebox.showerror(
+            "Error", "La variable ocean_proximity no puede usarse como variable ya que es una cadena de texto."
+        )
+        return
+
+    file_path = filedialog.asksaveasfilename(
+        defaultextension=".joblib",
+        filetypes=[("Joblib files", "*.joblib"), ("All files", "*.*")],
+        title="Guardar Modelo",
+    )
+
+    if not file_path:
+        return  
+
+    modelo = Modelo()
+
+    # Entrenar el modelo y asignarlo a la instancia
+    modelo.entrenar_modelo(lista_vo, list_vi, dataframe)
+
+    try:
+        # Guardar el modelo utilizando la clase Modelo
+        modelo.guardar_modelo(file_path)
+        tk.messagebox.showinfo("Éxito", f"Modelo guardado en: {file_path}")
+    except Exception as e:
+        tk.messagebox.showerror("Error", f"Error al guardar el modelo: {e}")
+
+def cargarModelo():
+    global opcion_seleccionada, estados_checkbuttons, dataframe, columnas,list_vi
+    # Seleccionar un archivo de modelo previamente guardado
+    filename = filedialog.askopenfilename(title="Seleccionar modelo", filetypes=[("Joblib files", "*.joblib"), ("All files", "*.*")])
+    if filename is not None:
+        # Crear una instancia de la clase Modelo
+        modelo = Modelo()
+        
+        # Cargar el modelo desde el archivo especificado
+        modelo_interno = modelo.cargar_modelo(filename)
+        
+        
+        if modelo_interno is not None:
+            # Obtener los coeficientes y el término independiente desde el modelo interno
+            error1, intercepto, coeficientes = modelo_interno
+              # Assuming the intercept is stored in the array
+            coeficientes = coeficientes.flatten().tolist()
+
+            # Ahora tendrás tus coeficientes como una lista con comas
+              
+            
+
+            getcontext().prec = 5  # Establecer la precisión a 5 decimales
+
+            ecuacion = "Y = "
+            for i, coef in enumerate(coeficientes):
+                ecuacion += f"({Decimal(str(coef)):.5g})*x{i+1} + "
+            ecuacion = ecuacion[:-2]  # Eliminar el último "+"
+
+            intercepto_decimal = Decimal(str(intercepto))  # Convertir a Decimal
+            ecuacion += f" + ({intercepto_decimal:.5g})"
+
+            # Mostrar detalles del modelo cargado
+            
+            print(f"El modelo {filename} tiene de error1: {error1}, intercepto: {intercepto}, y coeficientes: {coeficientes}")
+
+            # Crear una etiqueta para mostrar la ecuación en la ventana principal
+            ecuacion_label = tk.Label(ventana, text="Ecuación del modelo")
+            ecuacion_label.pack()
+            ecuacion_text = tk.Text(ventana, height=1, width=60)
+            ecuacion_text.insert(tk.END, str(ecuacion))
+            ecuacion_text.pack()
+        else:
+            print("El modelo interno es None. Revisa la carga del modelo.")
 
 def cerrar_programa():
     sys.exit()
-
-
-
-    
+ 
 def cargar_archivo():
     global archivo,dataframe
     archivo = filedialog.askopenfilename(title="Seleccionar archivo")
@@ -58,11 +159,7 @@ def cargar_archivo():
         dataframe = l.leer_archivo(archivo)
 
         if dataframe is not None:
-
-            # Crear un Frame para la tabla
-            frame_tabla = tk.Frame(ventana)
-            frame_tabla.pack(pady=10, padx=10)
-
+            limpiar_interfaz(1)
             # Crear un Treeview para mostrar la tabla
             treeview = ttk.Treeview(frame_tabla)
             treeview["columns"] = tuple(dataframe.columns)
@@ -87,14 +184,11 @@ def cargar_archivo():
             # Centrar la tabla en el Frame
             treeview.pack()
     crear_checkbuttons()
+
 def crear_checkbuttons():
     # Leer las columnas desde el archivo
     # Cambia 'ruta/del/archivo.csv' con la ruta correcta de tu archivo
-    global estados_checkbuttons, opcion_seleccionada,columnas, texto
-    frame_but2 = tk.Frame(ventana)
-    frame_but2.pack(padx=10,pady=3)
-    frame_but = tk.Frame(ventana)
-    frame_but.pack(pady=10, padx=3)
+    global estados_checkbuttons, opcion_seleccionada,columnas 
     datos=l.leer_archivo(archivo)
     columnas = list(datos.columns)
 
@@ -127,40 +221,92 @@ def crear_checkbuttons():
         radiobutton = ttk.Radiobutton(frame_but, text=columna, variable=opcion_seleccionada, value=columna)
         radiobutton.grid(row=1, column=i+1, sticky="w")
     boton_cargar = tk.Button(frame_but2, text="MOSTRAR MODELO", command=mostrar_modelo)
-    boton_cargar.grid(row=2,column=5,pady=5)
+    boton_cargar.grid(row=2,column=5,pady=3)
     boton_guardar = tk.Button(frame_but2, text="GUARDAR MODELO", command=guardar_modelo)
-    boton_guardar.grid(row=2,column=6,pady=5,padx=5)
-    texto=tk.StringVar()
-    etiqueta=tk.Label(frame_but2,text='Como:')
-    etiqueta.grid(row=2,column=7)
-    pantalla=tk.Entry(frame_but2, textvariable=texto)
-    pantalla.grid(row=2,column=8,columnspan=3,rowspan=2)
-    
-
-
+    boton_guardar.grid(row=2,column=6,pady=3,padx=5)
+     
+def limpiar_interfaz(n):
+    if n==1:
+        for widget in frame_tabla.winfo_children():
+            widget.destroy()
+        for widget in frame_grafica.winfo_children():
+            widget.destroy()
+        for widget in frame_but2.winfo_children():
+            widget.destroy()
+        for widget in frame_but.winfo_children():
+            widget.destroy()
+        for widget in frame_predicciones.winfo_children():
+            widget.destroy() 
+    elif n==2:
+        for widget in frame_grafica.winfo_children():
+            widget.destroy()
+        for widget in frame_predicciones.winfo_children():
+            widget.destroy() 
 # Crear ventana y otros elementos
+def on_horizontal_scroll(*args):
+    my_canvas.xview(*args)
+
+def predicciones(list_vi):
+    global my_canvas
 
 
+    
+    my_scrollbar=ttk.Scrollbar(frame_predicciones, orient="horizontal", command=on_horizontal_scroll)
+    my_scrollbar.pack(side='top',fill='x')
+    my_canvas= tk.Canvas(frame_predicciones,xscrollcommand=my_scrollbar.set)
+    my_canvas.pack(side='bottom',fill="both", expand=True)
+    second_frame= tk.Frame(my_canvas)
+    j=0
+    for i in list_vi:
+        
+        nombre=tk.Label(second_frame,text=str(i))
+        nombre.grid(column=j,row=0,padx=4)
+        j+=1
+        entrada_texto=tk.Entry(second_frame,state='normal')
+        entrada_texto.grid(column=j,row=0,padx=4)
+        j+=1
+    my_canvas.create_window((0,0), window=second_frame, anchor='nw')
+    second_frame.bind("<Configure>", lambda event, canvas=my_canvas: canvas.configure(scrollregion=my_canvas.bbox("all")))
 
-
+def cerrar_ventana():
+    ventana.destroy()
+    sys.exit()
 
 ventana = tk.Tk()
-
+ventana.protocol("WM_DELETE_WINDOW", cerrar_ventana)
 ancho_pantalla = ventana.winfo_screenwidth()
 alto_pantalla = ventana.winfo_screenheight()
 ventana.geometry(f"{ancho_pantalla}x{alto_pantalla}")
 
-entrada_texto = tk.Entry(ventana, state='disabled', width=40)
-entrada_texto.pack(pady=8)
+# Contenedor para la fila 0 (entrada de texto y botones)
+frame_top = tk.Frame(ventana)
+frame_top.pack(side="top", fill="x")
+
+entrada_texto = tk.Entry(frame_top, state='disabled', width=80)
+entrada_texto.grid(row=0,column=0,pady=3,padx=10)
+
+# Crear un Frame para la tabla
+frame_tabla = tk.Frame(ventana)
+frame_tabla.pack(pady=5, padx=10)
+
+# Crear un Frame para los botones de variables
+frame_but2 = tk.Frame(ventana)
+frame_but2.pack(padx=10,pady=3)
+frame_but = tk.Frame(ventana)
+frame_but.pack(pady=5, padx=3)
 
 # Botón para cargar un archivo
-boton_cargar = tk.Button(ventana, text="Cargar Archivo", command=cargar_archivo)
-boton_cargar.place(x=400,y=2)
-boton_cerrar = tk.Button(ventana, text="Cerrar Programa", command=cerrar_programa)
-boton_cerrar.place(x=1172,y=2)
+boton_elegir = tk.Button(frame_top, text="Elegir archivo", command=cargar_archivo)
+boton_elegir.grid(row=0, column=1, pady=3, padx=(10, 5), sticky='n', ipadx=10)
 
+boton_cargar = tk.Button(frame_top, text="Cargar modelo", command=cargarModelo)
+boton_cargar.grid(row=0, column=2, pady=3, padx=(5, 10), sticky='n', ipadx=10)
 
-
+# Crear un Frame para la grafica
+frame_grafica = tk.Frame(ventana,width=400, height=200)
+frame_grafica.pack()
+frame_predicciones=tk.Frame(ventana,padx=5,pady=5)
+frame_predicciones.pack(fill="both", expand=True)
 
 ventana.mainloop()
 
--- modelos.py
+++ modelos.py
@@ -4,84 +4,97 @@ import pandas as pd
 import seaborn as sb
 import numpy as np
 from sklearn.linear_model import LinearRegression
+from matplotlib.figure import Figure
 from sklearn.metrics import mean_squared_error
 from mpl_toolkits.mplot3d import Axes3D 
 from lector import *
+import joblib
+from sklearn.impute import SimpleImputer
 
 
-def regresion(columna_indep,columnas_dep,df, name=None):
+class Modelo:
+        def __init__(self):
+            self.modelo = None
 
-    
+        def entrenar_modelo(self, columna_indep, columnas_dep, df):
+            fig,error,formula,interc,coef = regresion(columna_indep, columnas_dep, df)
+            self.modelo = (error,interc,coef)
+            return self.modelo
+        def predecir(self, X):
+            if self.modelo is not None:
+                return self.modelo.predict(X)
+            else:
+                raise ValueError("El modelo no ha sido entrenado. Debes llamar a entrenar_modelo primero.")
 
-    df= df.dropna()
+        def guardar_modelo(self, path):
+            if self.modelo is not None:
+                joblib.dump(self.modelo,path)
+            else:
+                raise ValueError("El modelo no ha sido entrenado. Debes llamar a entrenar_modelo primero.")
 
+        def cargar_modelo(self, path):
+            try:
+            # Carga el modelo desde el archivo
+                modelo_cargado = joblib.load(path)
+                self.modelo = modelo_cargado
+                print("Modelo cargado correctamente. Tipo de objeto:", type(self.modelo))
+                return self.modelo
+            except Exception as e:
+                print(f"Error al cargar el modelo: {e}")
+                return None  # Asegúrate de retornar None si hay un error
+
+        def obtener_coeficientes(self):
+            if self.modelo is not None:
+                error, formula = self.modelo
+                return formula.coef_, formula.intercept_
+            else:
+                raise ValueError("El modelo no ha sido entrenado. Debes llamar a entrenar_modelo primero.")
+
+def regresion(columna_indep, columnas_dep, df, name=None):
+    df = df.dropna()
     indep_v = df[columna_indep].values
-
     x = len(columnas_dep)
-    dep_v = []
-    for i in range(x):
-        valores = df[columnas_dep[i]].values
-        dep_v.append(valores)
-
+    dep_v = [df[col].values for col in columnas_dep]
     X1 = np.array(dep_v).T
     Y1 = np.array(indep_v)
-
-
     reg = LinearRegression()
-
-    reg = reg.fit(X1,Y1)
-
+    reg = reg.fit(X1, Y1)
     y_pred = reg.predict(X1)
-
-
-    error = np.sqrt(mean_squared_error(Y1,y_pred))
-    r2 = reg.score(X1,Y1)
-    print("-"*36)
+    error = np.sqrt(mean_squared_error(Y1, y_pred))
+    r2 = reg.score(X1, Y1)
+    print("-" * 36)
     print("Error sin raíz: %.3f" % mean_squared_error(Y1, y_pred))
-    print("-"*36)
+    print("-" * 36)
     print("Error : ", error)
-    print("-"*36)
+    print("-" * 36)
     print("r2 es : ", r2)
-    print("-"*36)
-
-
+    print("-" * 36)
     coef = reg.coef_
-    interc ="{:.4f}".format(reg.intercept_[0])
-    
-    "{:.8f}".format(reg.intercept_[0])
-    for i  in range(len(coef[0])):
-        print("Beta " , str(i+1) ," =","{:.8f}".format(coef[0][i]))
+    interc = "{:.4f}".format(reg.intercept_[0])
+    for i in range(len(coef[0])):
+        print("Beta ", str(i + 1), " =", "{:.8f}".format(coef[0][i]))
         coef[0][i] = "{:.8f}".format(coef[0][i])
-    print("-"*36)
+    print("-" * 36)
     print('Término independiente: ', interc)
-    print("-"*36)
-    
+    print("-" * 36)
 
+    if len(columnas_dep) == 1:
+        fig = Figure(figsize=(5, 4), dpi=100)
+        ax = fig.add_subplot(111)
+        ax.scatter(X1, Y1, label='Datos', s=10)
+        ax.plot(X1, y_pred, color='red', label='Línea de regresión')
+        ax.set_xlabel(str(columna_indep[0]).upper())
+        ax.set_ylabel(str(columnas_dep[0]).upper())
+        leyend = "Y = " + "(" + str(coef[0][0]) + ")" + "*x1 + " + "(" + str(interc) + ")"
+        ax.set_title('Regresión Lineal Simple')
 
-    if len(columnas_dep) == 1 :
-        plt.scatter(X1, Y1, label='Datos' , s = 10)
-        plt.plot(X1, y_pred, color='red', label='Línea de regresión')
-        plt.xlabel(str(columna_indep[0]).upper())
-        plt.ylabel(str(columnas_dep[0]).upper())
-        leyend = "Y = "+ "(" + str(coef[0][0]) + ")" + "*x1 + " + "(" + str(interc) + ")"
-        plt.legend(title = leyend,title_fontsize =8    )
-        plt.title('Regresión Lineal Simple')
-        
-
-        if name !=None:
-    
-            plt.savefig(name)
-            
-            
-        elif name== None:
-
-            plt.show()
-
-
+        if name is not None:
+            fig.savefig(name)
+        else:
+            return fig,error,leyend,interc,coef
 
     elif len(columnas_dep) == 2:
-
-        fig = plt.figure()
+        fig = Figure(figsize=(4, 3), dpi=100)
         ax = fig.add_subplot(111, projection='3d')
         ax.scatter(dep_v[0], dep_v[1], Y1, c='r', marker='o')
         ax.set_xlabel((str(columnas_dep[0])).upper())
@@ -94,26 +107,51 @@ def regresion(columna_indep,columnas_dep,df, name=None):
         y_pred = reg.predict(np.vstack((x1_mesh.ravel(), x2_mesh.ravel())).T)
         y_pred = y_pred.reshape(x1_mesh.shape)
         ax.plot_surface(x1_mesh, x2_mesh, y_pred, alpha=0.5)
-        leyend = "Y = "+ "(" +str(  coef[0][0] ) + ")"+"*x1 + " + "(" + str(  coef[0][0]  ) +")" +"*x2 +"+"("  + str(   interc  ) + ")"
-        ax.legend(loc = 9,title_fontsize = 8 , title = leyend  )
-        
-        if name !=None:
-    
-            plt.savefig(name)
-            
-            
-        elif name== None:
+        leyend = "Y = " + "(" + str(coef[0][0]) + ")" + "*x1 + " + "(" + str(coef[0][0]) + ")" + "*x2 +" + "(" + str(
+            interc) + ")"
 
-            plt.show()
-
-
+        if name is not None:
+            fig.savefig(name)
+        else:
+            return fig,error,leyend,interc,coef
     elif len(columnas_dep) > 2:
         
-        formul = "Y = "
+        fig, axs = plt.subplots(len(columnas_dep), 1, figsize=(10, 15))
+        
+        formul = "Y = " 
         for i in range(len(coef[0])):
             formul += ("(" + str(coef[0][i])+")" + "*x" + str(i+1) + " + ") 
         formul += "("  + str(   interc  ) + ")"
-        print("Fórmula: \n",formul)
-        print("-"*36)
-    
-    
+
+        def f(n,coef,intercept):
+            x = 0
+            for i in range(len(n)):
+                x += n[i] * coef[0][i]
+            x += intercept
+            return x
+
+        
+        l = []
+        for i in range(len(coef[0])):
+            l.append(0)
+        
+        y1 = np.linspace(min(Y1),max(Y1),len(Y1))
+        print(Y1)
+        print(y1)
+        for i in range(len(l)):
+            l[i] = y1
+            z_values = f(l,coef,reg.intercept_[0])
+            axs[i].plot(y1,z_values)
+            title = "GRÁFICO " + str(i) + ": " + (str(columnas_dep[i]).upper()) + " y " + (str(columna_indep[0]).upper())
+            axs[i].set_title(title,fontsize = 6)
+            l[i] = 0
+            
+
+        plt.subplots_adjust(hspace = 0.5)
+        
+        if name is not None:
+            fig.savefig(name)
+        else:
+            interc=0
+            coef=0
+            return fig,error,formul,interc,coef
