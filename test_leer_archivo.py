b906d651c460d276b0cd8a9e51068b602af5b7cd
Jacobo Olmedo <jacobo.olmedo.sanchez@udc.es>
2023-12-21 10:42:29 +0100

    pruebas unitarias leer archivos

---
+++ test_leer_archivo.py
@@ -0,0 +1,25 @@
+import unittest
+import pandas as pd
+from lector import leer_archivo
+
+class TestLeerArchivo(unittest.TestCase):
+    def test_leer_archivo_con_archivo_csv(self):
+        ruta = 'housingcsv.csv'
+        df = leer_archivo(ruta)
+        self.assertIsInstance(df, pd.DataFrame)
+        self.assertIsNotNone(df)
+
+    def test_leer_archivo_con_archivo_xlsx(self):
+        ruta_xlsx = 'housing.xlsx'
+        df_xlsx = leer_archivo(ruta_xlsx)
+        self.assertIsInstance(df_xlsx, pd.DataFrame)
+        self.assertIsNotNone(df_xlsx)
+
+    def test_leer_archivo_con_archivo_db(self):
+        ruta_db = 'housing.db'
+        df_db = leer_archivo(ruta_db)
+        self.assertIsInstance(df_db, pd.DataFrame)
+        self.assertIsNotNone(df_db)
+
+if __name__ == '__main__':
+    unittest.main()
